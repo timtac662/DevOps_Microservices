1b1a3b0090af55099963009fb8655eddaca03f3c
Akintolu Timilehin <timtac662@gmail.com>
2022-09-14 16:53:55 +0000

    complete project 4

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="timtac66/api:v1.0.0"
+docker tag api timtac66/api:v1.0.0
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
